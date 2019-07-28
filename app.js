9f55731064dd69b962fb9b5f648f647045f74c2c
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-07-28 22:09:25 +0200

    new game function started

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
+newGame();
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+function newGame() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
 
-document.querySelector(".dice").style.display = "none";
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+}
+
+document.querySelector(".btn-new").addEventListener("click", newGame);
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
   //1.Radnom numeber
